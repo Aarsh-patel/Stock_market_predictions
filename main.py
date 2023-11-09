dec68b9d2293fb523a91debf8cb3aee11b04d1f0
Aarsh-patel <41568841+Aarsh-patel@users.noreply.github.com>
2023-11-09 18:02:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from ta.momentum import RSIIndicator
 import datetime
 
 
-model = tf.keras.models.load_model('/content/drive/MyDrive/Intel SIP/Crypto_Guides/Time_Sereies_model.h5')
+model = tf.keras.models.load_model('Time_Sereies_model.h5')
 def predict(df):
   ke = dict()
   pre = [None for i in df['Close'][:-160]]
